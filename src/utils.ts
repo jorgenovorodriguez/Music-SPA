1b6a673785c16c56f2462a8bf1f75822acefe9b6
HACKER <jorge.novo.rodriguez.93@gmail.com>
2024-11-06 19:28:09 +0100

    fix deploy imagesPNG

--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,6 @@
 export const getImageUrl = (path: string)  => {
     try {
-        return `public/images/${path}`; 
+        return `/images/${path}`; 
     } catch (error) {
         console.error(error);
     }
